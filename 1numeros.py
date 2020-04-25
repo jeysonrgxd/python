7182142cad84f552e3234cf2a074ff334fa690e3
jeyson <jeysonrgxd@gmail.com>
2020-04-25 02:37:08 -0500

    primer push de curso de python

---
+++ 1numeros.py
@@ -0,0 +1,6 @@
+a=1
+b=2
+#python define las operaciones de acuerdo a las reglas de las operacion multiplicacion y divicion primero
+c= a + b*3
+
+print(c)
---
+++ textos.py
@@ -0,0 +1,6 @@
+palabra = "HOLA JEYSON"
+palabra ="T"+palabra[1:]
+print(palabra)
+
+nombre = "casas"
+print(nombre[3:])
